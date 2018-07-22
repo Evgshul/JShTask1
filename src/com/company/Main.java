cefeb577478c4c4f44a9d010610a8620943ceda8
Evgenij <evgshul@gmail.com>
2018-07-23 00:09:15 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] mas = {1, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5};
+        int[] mas1 = new int[mas.length];
+
+        int b = 0;// quantity of repeat
+        int c = 0; //max repeat
+
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length; j++) {
+                if (mas[i] == mas[j]) {
+                    b++;
+                }
+            }
+            mas1[i] = b;
+            b = 0;
+        }
+
+        for (int i = 0; i < mas1.length; i++) {
+            if (mas[0] < mas1[i]) {
+                c = mas1[i];
+            }
+        }
+
+        int max = mas[0];
+        for (int i = 0; i < mas1.length; i++) {
+            if (c == mas1[i]) {
+                if (max < mas[i]) {
+                    max = mas[i];
+                }
+            }
+
+        }
+        System.out.println("integer from the longest recurring sequence within that array " + max);
+        System.out.println("longest recurring sequence " + c);
+    }
+}
+
+
+
