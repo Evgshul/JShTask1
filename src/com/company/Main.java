f7463b0ef1c996567e0c5b9642f3e503764bdf53
Evgenij <evgshul@gmail.com>
2018-07-30 12:30:34 +0300

    Add more variants to count longest and recurring sequence integer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] mas = {1, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5};
+        int[] mas = {1, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 7};
         int[] mas1 = new int[mas.length];
 
         int b = 0;// quantity of repeats
@@ -14,7 +14,6 @@ public class Main {
 
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas.length; j++) {
-
                 if (mas[i] == mas[j]) {
                     b++;
                 }
@@ -22,25 +21,29 @@ public class Main {
             mas1[i] = b;
             b = 0;
         }
-        System.out.println();
-        for (int i = 0; i < mas1.length; i++) {
-            if (mas1[0] < mas1[i]) {
-                c = mas1[i];
-            }
-        }
 
-        int max = mas[4];
-        for (int i = 0; i < mas1.length; i++) {
-            if (c == mas1[i]) {
-                if (max < mas[i]) {
-                    max = mas[i];
-                }
-            }
+        for (int i : mas1) if (mas1[0] < mas1[i]) c = mas1[i];
+
+        int max = mas[0];
+        int k = mas[0];
+        for (int i : mas) {
+            if (c == mas1[i] && max < mas[i]) max = mas[i];
+            if (c == mas1[i]) {k = Math.max(k, mas[i]);}
         }
         System.out.println("integer from the longest recurring sequence within that array " + max);
         System.out.println("longest recurring sequence " + c);
+        System.out.println("another metods to find longest recurring sequence " + k);
+
+
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] == max) {
+                System.out.printf("%d", i);
+
+            }
+        }
     }
 }
+
 
 
 
