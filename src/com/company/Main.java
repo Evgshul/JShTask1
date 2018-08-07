f7a4c89c49edda5bbbc5f5797c14c7bee895b86c
Evgenij <evgshul@gmail.com>
2018-08-07 23:32:34 +0300

    linear traversal finding max reapeat integer in one loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -35,4 +35,5 @@ public class Main {
         System.out.println("max repeat " + max_repeat);
         System.out.println("integer from the longest recurring sequence within that array " + a);
     }
+
 }
