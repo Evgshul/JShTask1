b4f8b1634f61d08c16ceb64873fec68aee6d0dc8
Evgenij <evgshul@gmail.com>
2018-08-08 12:27:09 +0300

    aaa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -34,7 +34,5 @@ public class Main {
         System.out.println();
         System.out.println("max repeat " + max_repeat);
         System.out.println("integer from the longest recurring sequence within that array " + a);
-
-
     }
 }
